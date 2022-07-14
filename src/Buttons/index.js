5725b1bea4e32b5760129c30b305c048eb537c47
Jakub Bogdanski <bogdanskikuba@gmail.com>
2022-07-14 10:44:34 +0200

    added React.Fragment

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,14 +1,20 @@
+import React from "react";
 import "./style.css"
 
 const Buttons = ({ tasks, hideDoneTask }) => (
-   tasks.length > 0 && (
-        <div>
-            <button className="section__taskListHeaderButton"> {hideDoneTask ? " Pokaż " : " Ukryj "}ukończone </button>
-            <button className="section__taskListHeaderButton"
-                disabled={tasks.every(({ done }) => done)}
-            >
-                Ukończ wszystkie</button>
-        </div>
-    )
+
+    <div>
+        {tasks.length > 0 && (
+            <React.Fragment>
+                <button className="section__taskListHeaderButton"> {hideDoneTask ? " Pokaż " : " Ukryj "}ukończone </button>
+                <button className="section__taskListHeaderButton"
+                    disabled={tasks.every(({ done }) => done)}
+                >
+                    Ukończ wszystkie</button>
+            </React.Fragment>
+        )}
+    </div>
+
+
 );
 export default Buttons;
